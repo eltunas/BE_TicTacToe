89c72b91f57ba61fdaa29ca41c40c5faa4910121
Tomas <tgargiulo4@gmail.com>
2020-06-02 00:04:57 -0300

    sockets room and queue intial set up

--- socketApi.js
+++ socketApi.js
@@ -5,13 +5,42 @@ let socketApi = {};
 socketApi.io = io;
 module.exports = socketApi;
 
+let queue = [];
+
 socketApi.io.on("connection", socket => {
     
-    console.log("user connected");
+    socket.on("findMatch", (data) => {
+        findMatch(socket);
 
-    socket.on("play", (play) => {
-        console.log(play)
-        socket.emit('played', play);
+        socket.on("play", (play) => {
+            console.log(play)
+            socket.emit('played', play);
+        });
+
     })
 
+
+    
+
 });
+
+
+function findMatch(socket){
+    if (queue) {
+        // somebody is in queue, pair them!
+        let peer = queue.pop();
+        let room = socket.id + '#' + peer.id;
+        // join them both
+        peer.join(room);
+        socket.join(room);
+        // register rooms to their names
+        rooms[peer.id] = room;
+        rooms[socket.id] = room;
+        // exchange names between the two of them and start the chat
+        peer.emit('chat start', {'name': names[socket.id], 'room':room});
+        socket.emit('chat start', {'name': names[peer.id], 'room':room});
+    } else {
+        // queue is empty, add our lone socket
+        queue.push(socket);
+    }
+}
