6ea6e4a0f11e98b8c3c01c99d665bc2836b46972
Tomas <tgargiulo4@gmail.com>
2020-06-02 22:12:09 -0300

    socket queue first implementation

--- socketApi.js
+++ socketApi.js
@@ -6,40 +6,48 @@ socketApi.io = io;
 module.exports = socketApi;
 
 let queue = [];
+let rooms = [];
 
 socketApi.io.on("connection", socket => {
-    
-    socket.on("findMatch", (data) => {
+    console.log("conecto");
+    console.log(queue);
+    socket.on("findMatch", () => {
+        //validar que no este en un match en la queue
         findMatch(socket);
+        socket.emit('matchFound', null);
+    });
 
-        socket.on("play", (play) => {
-            console.log(play)
-            socket.emit('played', play);
-        });
-
-    })
-
-
-    
-
+    socket.on("play", (play) => {
+        console.log(play)
+        socket.emit('played', play);
+    });
 });
 
 
 function findMatch(socket){
-    if (queue) {
+    console.log(queue);
+    
+    if (queue.length > 0) {
+        console.log("match found")
         // somebody is in queue, pair them!
         let peer = queue.pop();
-        let room = socket.id + '#' + peer.id;
-        // join them both
-        peer.join(room);
-        socket.join(room);
-        // register rooms to their names
-        rooms[peer.id] = room;
-        rooms[socket.id] = room;
-        // exchange names between the two of them and start the chat
-        peer.emit('chat start', {'name': names[socket.id], 'room':room});
-        socket.emit('chat start', {'name': names[peer.id], 'room':room});
+        if(peer.id == socket.id){
+            queue.push(peer);
+        }else{
+            let room = socket.id + '#' + peer.id;
+            // join them both
+            peer.join(room);
+            socket.join(room);
+            // register rooms to their names
+            rooms[peer.id] = room;
+            rooms[socket.id] = room;
+            // exchange names between the two of them and start the chat
+            console.log("matcheando")
+            peer.emit("maaatchFound");
+            socket.emit("maaatchFound");
+        }
     } else {
+        console.log("busco partida " + socket.id);
         // queue is empty, add our lone socket
         queue.push(socket);
     }
