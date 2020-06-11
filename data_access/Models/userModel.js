900f03811a5063bd7237d007e2e370d4258c7697
Sandoval German <GS25398@GSCorp.AD>
2020-06-11 00:26:25 -0300

    fix name 2

---
+++ data_access/Models/userModel.js
@@ -0,0 +1,14 @@
+class User {
+  constructor(googleId, name, createdDate) {
+    this.googleId = googleId;
+    this.name = name;
+    this.createdDate = createdDate;
+    this.wins = 0;
+    this.ties = 0;
+    this.losses = 0;
+  }
+}
+
+//method for User entity data access
+
+module.exports = { User };
