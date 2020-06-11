3cd4230b88de87b169ddbc7cb662095c8b594a93
Sandoval German <GS25398@GSCorp.AD>
2020-06-11 02:01:55 -0300

    online and queue api endpoints to get one and delete one

--- data_access/queueUsers.js
+++ data_access/queueUsers.js
@@ -1,34 +1,44 @@
 const connection = require("./mongo_connection");
 
 async function getQueueUsers() {
-    const clientmongo = await connection.getConnection();
-    const collection = await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("Queue")
-        .find()
-        .toArray();
-    return collection;
+  const clientmongo = await connection.getConnection();
+  const collection = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("Queue")
+    .find()
+    .toArray();
+  return collection;
+}
+
+async function getQueueUser(googleId) {
+  const clientmongo = await connection.getConnection();
+  const user = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("Queue")
+    .findOne({ googleId: googleId.toString() });
+  return user;
 }
 
 async function insertQueueUser(QueueUser) {
-    const clientmongo = await connection.getConnection();
-    const { ops } = await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("Queue")
-        .insertOne(QueueUser);
-    return ops;
+  const clientmongo = await connection.getConnection();
+  const { ops } = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("Queue")
+    .insertOne(QueueUser);
+  return ops;
 }
 
 async function deleteQueueUser(googleId) {
-    const clientmongo = await connection.getConnection();
-    await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("Queue")
-        .deleteOne({ googleId: googleId.toString() });
+  const clientmongo = await connection.getConnection();
+  await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("Queue")
+    .deleteOne({ googleId: googleId.toString() });
 }
 
 module.exports = {
-    getQueueUsers,
-    insertQueueUser,
-    deleteQueueUser,
+  getQueueUsers,
+  getQueueUser,
+  insertQueueUser,
+  deleteQueueUser,
 };
--- routes/queueUsers.js
+++ routes/queueUsers.js
@@ -1,31 +1,65 @@
 const express = require("express");
 const router = express.Router();
 const queueUsers = require("../data_access/queueUsers");
-const queueUserModel = require("../data_access/Models/queueUserModel")
+const queueUserModel = require("../data_access/Models/queueUserModel");
 
 router.get("/", async (req, res) => {
-    try {
-        const users = await queueUsers.getQueueUsers();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
+  try {
+    const users = await queueUsers.getQueueUsers();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/:id", getQueueUser, (req, res) => {
+  res.json(res.user);
 });
 
 router.post("/", async (req, res) => {
-    let user;
-    if (req.body.googleId == null || req.body.name == null || req.body.socket == null) {
-        return res.status(400).json({ message: "Parametros incorrectos" })
-    } else {
-        user = new queueUserModel.QueueUser(req.body.googleId, req.body.name, req.body.socket)
-    }
-    try {
-        const newUser = await queueUsers.insertQueueUser(user);
-        res.status(201).json(newUser);
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
+  let user;
+  if (
+    req.body.googleId == null ||
+    req.body.name == null ||
+    req.body.socket == null
+  ) {
+    return res.status(400).json({ message: "Parametros incorrectos" });
+  } else {
+    user = new queueUserModel.QueueUser(
+      req.body.googleId,
+      req.body.name,
+      req.body.socket
+    );
+  }
+  try {
+    const newUser = await queueUsers.insertQueueUser(user);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
+router.delete("/:id", getQueueUser, async (req, res) => {
+  try {
+    await queueUsers.deleteQueueUser(res.user.googleId);
+    res.status(200).json({ message: `Deleted user ${res.user.googleId}` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getQueueUser(req, res, next) {
+  let user;
+  try {
+    user = await queueUsers.getQueueUser(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: "cannot find queue user" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.user = user;
+  next();
+}
 
 module.exports = router;
