3ebff8489562139a90c4101289b4109f19c30176
Sandoval German <GS25398@GSCorp.AD>
2020-06-12 17:23:11 -0300

    return object instead of array in insert online user and room

--- data_access/onlineUsers.js
+++ data_access/onlineUsers.js
@@ -1,45 +1,44 @@
 const connection = require("./mongo_connection");
 
 async function getOnlineUsers() {
-    const clientmongo = await connection.getConnection();
-    const collection = await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("OnlineUsers")
-        .find()
-        .toArray();
-    return collection;
+  const clientmongo = await connection.getConnection();
+  const collection = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("OnlineUsers")
+    .find()
+    .toArray();
+  return collection;
 }
 
 async function getOnlineUser(googleId) {
-    const clientmongo = await connection.getConnection();
-    const user = await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("OnlineUsers")
-        .findOne({ googleId: googleId.toString() });
-    return user;
+  const clientmongo = await connection.getConnection();
+  const user = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("OnlineUsers")
+    .findOne({ googleId: googleId.toString() });
+  return user;
 }
 
 async function insertOnlineUser(onlineUser) {
-    const clientmongo = await connection.getConnection();
-    const { ops } = await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("OnlineUsers")
-        .insertOne(onlineUser);
-    return ops;
+  const clientmongo = await connection.getConnection();
+  const { ops } = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("OnlineUsers")
+    .insertOne(onlineUser);
+  return ops[0];
 }
 
 async function deleteOnlineUser(googleId) {
-    const clientmongo = await connection.getConnection();
-    await clientmongo
-        .db("db_tic_tac_toe")
-        .collection("OnlineUsers")
-        .deleteOne({ googleId: googleId.toString() });
+  const clientmongo = await connection.getConnection();
+  await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("OnlineUsers")
+    .deleteOne({ googleId: googleId.toString() });
 }
 
-
 module.exports = {
-    getOnlineUsers,
-    getOnlineUser,
-    insertOnlineUser,
-    deleteOnlineUser,
+  getOnlineUsers,
+  getOnlineUser,
+  insertOnlineUser,
+  deleteOnlineUser,
 };
--- data_access/rooms.js
+++ data_access/rooms.js
@@ -1,45 +1,52 @@
 const connection = require("./mongo_connection");
 
 async function getRoomByPlayerId(playerId) {
-    const clientmongo = await connection.getConnection();
-    const room = await clientmongo
-      .db("db_tic_tac_toe")
-      .collection("Rooms")
-      .findOne({ $or: [ { player1Id: playerId.toString() }, { player2Id: playerId.toString() } ] });
+  const clientmongo = await connection.getConnection();
+  const room = await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("Rooms")
+    .findOne({
+      $or: [
+        { player1Id: playerId.toString() },
+        { player2Id: playerId.toString() },
+      ],
+    });
 
-    console.log(room);
-      
-    return room;
+  console.log(room);
+
+  return room;
 }
-  
+
 async function insertRoom(room) {
-    const clientmongo = await connection.getConnection();
-    const { ops } = await clientmongo
+  const clientmongo = await connection.getConnection();
+  const { ops } = await clientmongo
     .db("db_tic_tac_toe")
     .collection("Rooms")
     .insertOne(room);
-    return ops;
+  return ops[0];
 }
-  
+
 async function deleteRoom(roomId) {
-    const clientmongo = await connection.getConnection();
-    await clientmongo
+  const clientmongo = await connection.getConnection();
+  await clientmongo
     .db("db_tic_tac_toe")
     .collection("Rooms")
     .deleteOne({ id: roomId.toString() });
 }
 
 async function updateRoom(room) {
-    const clientmongo = await connection.getConnection();
-    const query = { id: room.id.toString() };
-    const newValues = { $set: { 
-                            nextToMove: room.nextToMove.toString(),
-                            boardState: room.boardState
-                         } };
-    await clientmongo
-      .db("db_tic_tac_toe")
-      .collection("Rooms")
-      .updateOne(query, newValues);
+  const clientmongo = await connection.getConnection();
+  const query = { id: room.id.toString() };
+  const newValues = {
+    $set: {
+      nextToMove: room.nextToMove.toString(),
+      boardState: room.boardState,
+    },
+  };
+  await clientmongo
+    .db("db_tic_tac_toe")
+    .collection("Rooms")
+    .updateOne(query, newValues);
 }
 
-module.exports = {getRoomByPlayerId, insertRoom, deleteRoom, updateRoom}
+module.exports = { getRoomByPlayerId, insertRoom, deleteRoom, updateRoom };
