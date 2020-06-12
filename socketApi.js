1e9fef14294de4b07eea73b75d08e228271b8dfd
Sandoval German <GS25398@GSCorp.AD>
2020-06-11 21:16:29 -0300

    fix

--- socketApi.js
+++ socketApi.js
@@ -1,6 +1,5 @@
-
-const gameLogic = require('./helpers/gameLogicHelper')
-const socket_io = require('socket.io');
+const gameLogic = require("./helpers/gameLogicHelper");
+const socket_io = require("socket.io");
 const io = socket_io();
 const dataRooms = require("./data_access/rooms");
 const RoomModel = require("./data_access/Models/RoomModel");
@@ -17,70 +16,68 @@ socketApi.io.on("connection", socket => {
     socket.on("move", moveData => {
       let room = rooms[moveData.socketId];
 
+      socket.on("move", async moveData => {
+        let room = await dataRooms.getRoomByPlayerId(moveData.socketId);
 
-        socket.on("move", async moveData => {            
-            let room = await dataRooms.getRoomByPlayerId(moveData.socketId);
-            
-            console.log(room);
-            
-            room.boardState = updateBoard(room, moveData.square);
+        console.log(room);
 
-            let matchWon = gameLogic.gameWon(room.boardState, room.nextToMove);
+        room.boardState = updateBoard(room, moveData.square);
 
-            room.nextToMove = room.nextToMove == "X" ? "O" : "X";
+        let matchWon = gameLogic.gameWon(room.boardState, room.nextToMove);
 
-            await dataRooms.updateRoom(room);
-            
-            socketApi.io.in(room.id).emit("boardUpdate", room);
-            
-            if(matchWon){
-                console.log("match ended");
-                socketApi.io.in(room.id).emit("matchEnded", "");
-                dataRooms.deleteRoom(room.id);
-            }
-        })
+        room.nextToMove = room.nextToMove == "X" ? "O" : "X";
+
+        await dataRooms.updateRoom(room);
+
+        socketApi.io.in(room.id).emit("boardUpdate", room);
+
+        if (matchWon) {
+          console.log("match ended");
+          socketApi.io.in(room.id).emit("matchEnded", "");
+          dataRooms.deleteRoom(room.id);
+        }
+      });
     });
-});
+  });
 
-function findMatch(socket) {
-  if (queue.length > 0) {
-    console.log("match found");
-    // somebody is in queue, pair them!
-    let peer = queue.pop();
-    if (peer.id == socket.id) {
-      queue.push(peer);
-    } else {
-      let player1;
-      let player2;
-
-      if (Math.random > 0.5) {
-        player1 = "X";
-        player2 = "O";
+  function findMatch(socket) {
+    if (queue.length > 0) {
+      console.log("match found");
+      // somebody is in queue, pair them!
+      let peer = queue.pop();
+      if (peer.id == socket.id) {
+        queue.push(peer);
       } else {
-        player1 = "O";
-        player2 = "X";
+        let player1;
+        let player2;
+
+        if (Math.random > 0.5) {
+          player1 = "X";
+          player2 = "O";
+        } else {
+          player1 = "O";
+          player2 = "X";
+        }
+
+        let room = new RoomModel.Room(socket.id + "#" + peer.id, peer, socket);
+
+        dataRooms.insertRoom(room);
+
+        // join them both
+        peer.join(room.id);
+        socket.join(room.id);
+
+        peer.emit("matchFound", player1);
+        socket.emit("matchFound", player2);
       }
-      
-      let room = new RoomModel.Room(socket.id + '#' + peer.id, peer, socket);
-            
-      dataRooms.insertRoom(room);
-      
-      // join them both
-      peer.join(room.id);
-      socket.join(room.id);
-      
-      
-      peer.emit("matchFound", player1);
-      socket.emit("matchFound", player2);
-
+    } else {
+      // queue is empty, add our lone socket
+      queue.push(socket);
     }
-  } else {
-    // queue is empty, add our lone socket
-    queue.push(socket);
   }
-}
 
-function updateBoard(room, newMove) {
-  room.boardState[newMove] = room.nextToMove;
-  return room.boardState;
-}
+  function updateBoard(room, newMove) {
+    room.boardState[newMove] = room.nextToMove;
+    return room.boardState;
+  }
+});
