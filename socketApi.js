18e60b31ccb34c69eb376a60093a00949b4402d9
Sandoval German <GS25398@GSCorp.AD>
2020-06-16 19:24:14 -0300

    try 11

--- socketApi.js
+++ socketApi.js
@@ -13,51 +13,52 @@ let queue = [];
 socketApi.io.on("connection", socket => {
   socket.on("findMatch", () => {
     findMatch(socket);
-    socket.on("move", async function moveData(moveData) {
-      let room = await dataRooms.getRoomByPlayerId(moveData.socketId);
-
-      room.boardState = updateBoard(room, moveData.square);
-
-      let moves = room.boardState.filter(square => square != null).length;
-
-      let winner = moves > 4 ? gameLogic.gameWon(room.boardState) : null;
-
-      room.nextToMove = room.nextToMove == "X" ? "O" : "X";
-
-      await dataRooms.updateRoom(room);
-
-      socketApi.io.in(room.id).emit("boardUpdate", room);
-
-      if (winner || moves === 9) {
-        console.log("match ended");
-
-        socketApi.io.in(room.id).emit("matchEnded", winner ? winner : null);
-        console.log(
-          "Before removing sockets from room: ",
-          io.sockets.adapter.rooms
-        );
-
-        socketApi.io.in(room.id).clients((error, clients) => {
-          if (clients.length > 0) {
-            console.log("clients in the room: \n");
-            console.log(clients);
-            clients.forEach(socket_id => {
-              io.sockets.sockets[socket_id].leave(room.id);
-              io.sockets.sockets[socket_id].removeListener("move", this);
-              console.log(io.sockets.sockets[socket_id].eventNames());
-            });
-          }
-        });
-
-        await dataRooms.deleteRoom(room.id);
-        console.log(
-          "After removing sockets from room: ",
-          io.sockets.adapter.rooms
-        );
-      }
-    });
+    socket.on("move", moveData => moveData(moveData));
   });
 });
+
+const moveData = (moveData) => {
+  let room = await dataRooms.getRoomByPlayerId(moveData.socketId);
+
+  room.boardState = updateBoard(room, moveData.square);
+
+  let moves = room.boardState.filter(square => square != null).length;
+
+  let winner = moves > 4 ? gameLogic.gameWon(room.boardState) : null;
+
+  room.nextToMove = room.nextToMove == "X" ? "O" : "X";
+
+  await dataRooms.updateRoom(room);
+
+  socketApi.io.in(room.id).emit("boardUpdate", room);
+
+  if (winner || moves === 9) {
+    console.log("match ended");
+
+    socketApi.io.in(room.id).emit("matchEnded", winner ? winner : null);
+    console.log(
+      "Before removing sockets from room: ",
+      io.sockets.adapter.rooms
+    );
+
+    socketApi.io.in(room.id).clients((error, clients) => {
+      if (clients.length > 0) {
+        console.log("clients in the room: \n");
+        console.log(clients);
+        clients.forEach(socket_id => {
+          io.sockets.sockets[socket_id].leave(room.id);
+          io.sockets.sockets[socket_id].removeListener("move", this);
+          console.log(io.sockets.sockets[socket_id].eventNames());
+        });
+      }
+    });
+
+    await dataRooms.deleteRoom(room.id);
+    console.log(
+      "After removing sockets from room: ",
+      io.sockets.adapter.rooms
+    );
+  }
 
 function findMatch(socket) {
   if (queue.length > 0) {
