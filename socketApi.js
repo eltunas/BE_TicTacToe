c97bf3f41ab23218f3d09f65a9cdfddcd8303dc1
Sandoval German <GS25398@GSCorp.AD>
2020-06-16 20:25:11 -0300

    try 24

--- socketApi.js
+++ socketApi.js
@@ -50,8 +50,7 @@ const moveData = async moveData => {
         clients.forEach(socket_id => {
           socketApi.io.sockets.sockets[socket_id].leave(room.id);
           socketApi.io.sockets.sockets[socket_id].removeAllListeners();
-          socketApi.io.sockets.sockets[socket_id].on(
-            "findMatch",
+          socketApi.io.sockets.sockets[socket_id].on("findMatch", () =>
             findMatch(socketApi.io.sockets.sockets[socket_id])
           );
           console.log(
