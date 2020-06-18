0ce216050f5733a2ebb3df088df84f6e87ac6bfe
Sandoval German <GS25398@GSCorp.AD>
2020-06-18 16:18:59 -0300

    remove toString

--- data_access/Models/RoomModel.js
+++ data_access/Models/RoomModel.js
@@ -1,11 +1,11 @@
 //prettier-ignore
 class Room {
     constructor(roomId, player1, player2) {  
-        this.id = roomId.toString(),
+        this.id = roomId,
         this.boardState= Array(9).fill(null);
-        this.nextToMove = "X".toString(),
-        this.player1Id = player1.id.toString(),
-        this.player2Id = player2.id.toString(),
+        this.nextToMove = "X",
+        this.player1Id = player1.id,
+        this.player2Id = player2.id,
         this.moves = 0;
     }
 }
