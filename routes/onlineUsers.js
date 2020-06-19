616407dbfd134984854011d730be701f1dcfbf72
Sandoval German <GS25398@GSCorp.AD>
2020-06-19 14:04:25 -0300

    fix fns

--- routes/onlineUsers.js
+++ routes/onlineUsers.js
@@ -12,4 +12,37 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", getDuplicateUser, async (req, res) => {
+  let user;
+  if (
+    req.body.googleId == null ||
+    req.body.name == null ||
+    req.body.socketId == null
+  ) {
+    return res.status(400).json({ message: "Parametros incorrectos" });
+  } else {
+    user = new onlineUserModel.OnlineUser(
+      req.body.googleId,
+      req.body.name,
+      req.body.socketId
+    );
+  }
+  try {
+    const newUser = await onlineUsers.insertOnlineUser(user);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getDuplicateUser(req, res, next) {
+  let user;
+  try {
+    user = await onlineUsers.getOnlineUser(req.body.googleId);
+    return user != null ? res.status(200).json(user) : next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
 module.exports = router;
