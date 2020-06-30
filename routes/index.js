8b7bf93c54e3467c0daa3517073d43775d23495a
Sandoval German <GS25398@GSCorp.AD>
2020-06-30 17:25:09 -0300

    forgot some slashs

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ var express = require("express");
 var router = express.Router();
 const auth = require("../Middlewares/auth");
 
-//router.use(async(req, res, next) => await auth.verifyToken(req, res, next));
-
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
